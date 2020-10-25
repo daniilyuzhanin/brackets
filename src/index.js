5dabf80db476b3a30072db4baddd1c7afe3f0913
Daniil Yuzhanin <daniilyuzhanin@gmail.com>
2020-10-25 21:02:50 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let first = bracketsConfig.map((arr) => arr.join(''));
+  let second = str;
+	for (let i = 0; i < str.length; i++) {
+		for (let j = 0; j < first.length; j++) {
+			second = second.replace(first[j], '');
+		}
+	}
+	if (second == '') {
+		return true;
+	} else {
+		return false;
+	}
 }
